bb34955ab55786d8cb178e6df2c7f940ec73b95b
kunnchan <barbiq36@gmail.com>
2020-06-29 21:46:48 +0800

    update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,280 +1,258 @@
-CREATE DATABASE  IF NOT EXISTS `onlineshopping` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `onlineshopping`;
+--CREATE DATABASE  IF NOT EXISTS `onlineshopping`;
+--USE `onlineshopping`;
 -- MySQL dump 10.13  Distrib 8.0.19, for macos10.15 (x86_64)
 --
 -- Host: localhost    Database: onlineshopping
 -- ------------------------------------------------------
 -- Server version	8.0.19
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
 --
 -- Table structure for table `billing_address`
 --
 
-DROP TABLE IF EXISTS `billing_address`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `billing_address` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `created_by` varchar(255) DEFAULT NULL,
-  `created_date` datetime DEFAULT NULL,
-  `updated_by` varchar(255) DEFAULT NULL,
-  `updated_date` datetime DEFAULT NULL,
-  `active` bit(1) DEFAULT NULL,
-  `address_line` varchar(255) DEFAULT NULL,
-  `city` varchar(255) DEFAULT NULL,
-  `country` varchar(255) DEFAULT NULL,
-  `full_name` varchar(255) DEFAULT NULL,
-  `phone` varchar(255) DEFAULT NULL,
-  `postal` varchar(255) DEFAULT NULL,
-  `state` varchar(255) DEFAULT NULL,
-  `user_id` bigint DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FKdu3ltqxxsqs7ah8ubnkt12q48` (`user_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+--DROP TABLE IF EXISTS `billing_address`;
+--CREATE TABLE `billing_address` (
+--  `id` bigint NOT NULL AUTO_INCREMENT,
+--  `created_by` varchar(255) DEFAULT NULL,
+--  `created_date` datetime DEFAULT NULL,
+--  `updated_by` varchar(255) DEFAULT NULL,
+--  `updated_date` datetime DEFAULT NULL,
+--  `active` bit(1) DEFAULT NULL,
+--  `address_line` varchar(255) DEFAULT NULL,
+--  `city` varchar(255) DEFAULT NULL,
+--  `country` varchar(255) DEFAULT NULL,
+--  `full_name` varchar(255) DEFAULT NULL,
+--  `phone` varchar(255) DEFAULT NULL,
+--  `postal` varchar(255) DEFAULT NULL,
+--  `state` varchar(255) DEFAULT NULL,
+--  `user_id` bigint DEFAULT NULL,
+--  PRIMARY KEY (`id`),
+--  KEY `FKdu3ltqxxsqs7ah8ubnkt12q48` (`user_id`)
+--) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+--/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `billing_address`
 --
 
-LOCK TABLES `billing_address` WRITE;
-/*!40000 ALTER TABLE `billing_address` DISABLE KEYS */;
-/*!40000 ALTER TABLE `billing_address` ENABLE KEYS */;
-UNLOCK TABLES;
+--LOCK TABLES `billing_address` WRITE;
+--/*!40000 ALTER TABLE `billing_address` DISABLE KEYS */;
+--/*!40000 ALTER TABLE `billing_address` ENABLE KEYS */;
+--UNLOCK TABLES;
 
 --
 -- Table structure for table `cart`
 --
 
-DROP TABLE IF EXISTS `cart`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `cart` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `created_by` varchar(255) DEFAULT NULL,
-  `created_date` datetime DEFAULT NULL,
-  `updated_by` varchar(255) DEFAULT NULL,
-  `updated_date` datetime DEFAULT NULL,
-  `price` double DEFAULT NULL,
-  `product_id` bigint DEFAULT NULL,
-  `qty` int NOT NULL,
-  `user_id` bigint DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+--DROP TABLE IF EXISTS `cart`;
+--/*!40101 SET @saved_cs_client     = @@character_set_client */;
+--/*!50503 SET character_set_client = utf8mb4 */;
+--CREATE TABLE `cart` (
+--  `id` bigint NOT NULL AUTO_INCREMENT,
+--  `created_by` varchar(255) DEFAULT NULL,
+--  `created_date` datetime DEFAULT NULL,
+--  `updated_by` varchar(255) DEFAULT NULL,
+--  `updated_date` datetime DEFAULT NULL,
+--  `price` double DEFAULT NULL,
+--  `product_id` bigint DEFAULT NULL,
+--  `qty` int NOT NULL,
+--  `user_id` bigint DEFAULT NULL,
+--  PRIMARY KEY (`id`)
+--) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+--/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `cart`
 --
 
-LOCK TABLES `cart` WRITE;
-/*!40000 ALTER TABLE `cart` DISABLE KEYS */;
-/*!40000 ALTER TABLE `cart` ENABLE KEYS */;
-UNLOCK TABLES;
+--LOCK TABLES `cart` WRITE;
+--/*!40000 ALTER TABLE `cart` DISABLE KEYS */;
+--/*!40000 ALTER TABLE `cart` ENABLE KEYS */;
+--UNLOCK TABLES;
 
 --
 -- Table structure for table `history`
 --
 
-DROP TABLE IF EXISTS `history`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `history` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `action` varchar(255) DEFAULT NULL,
-  `content` longtext,
-  `modified_by` varchar(255) DEFAULT NULL,
-  `modified_date` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+--DROP TABLE IF EXISTS `history`;
+--/*!40101 SET @saved_cs_client     = @@character_set_client */;
+--/*!50503 SET character_set_client = utf8mb4 */;
+--CREATE TABLE `history` (
+--  `id` int NOT NULL AUTO_INCREMENT,
+--  `action` varchar(255) DEFAULT NULL,
+--  `content` longtext,
+--  `modified_by` varchar(255) DEFAULT NULL,
+--  `modified_date` datetime DEFAULT NULL,
+--  PRIMARY KEY (`id`)
+--) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+--/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `history`
 --
 
-LOCK TABLES `history` WRITE;
-/*!40000 ALTER TABLE `history` DISABLE KEYS */;
-/*!40000 ALTER TABLE `history` ENABLE KEYS */;
-UNLOCK TABLES;
+--LOCK TABLES `history` WRITE;
+--/*!40000 ALTER TABLE `history` DISABLE KEYS */;
+--/*!40000 ALTER TABLE `history` ENABLE KEYS */;
+--UNLOCK TABLES;
 
 --
 -- Table structure for table `order_detail`
 --
 
-DROP TABLE IF EXISTS `order_detail`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `order_detail` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `created_by` varchar(255) DEFAULT NULL,
-  `created_date` datetime DEFAULT NULL,
-  `updated_by` varchar(255) DEFAULT NULL,
-  `updated_date` datetime DEFAULT NULL,
-  `product_id` bigint DEFAULT NULL,
-  `qty` int NOT NULL,
-  `order_id` bigint DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK4y2bf6ywxd8esiqy1c55mtcma` (`order_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
+--DROP TABLE IF EXISTS `order_detail`;
+--/*!40101 SET @saved_cs_client     = @@character_set_client */;
+--/*!50503 SET character_set_client = utf8mb4 */;
+--CREATE TABLE `order_detail` (
+--  `id` bigint NOT NULL AUTO_INCREMENT,
+--  `created_by` varchar(255) DEFAULT NULL,
+--  `created_date` datetime DEFAULT NULL,
+--  `updated_by` varchar(255) DEFAULT NULL,
+--  `updated_date` datetime DEFAULT NULL,
+--  `product_id` bigint DEFAULT NULL,
+--  `qty` int NOT NULL,
+--  `order_id` bigint DEFAULT NULL,
+--  PRIMARY KEY (`id`),
+--  KEY `FK4y2bf6ywxd8esiqy1c55mtcma` (`order_id`)
+--) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+--/*!40101 SET character_set_client = @saved_cs_client */;
 --
--- Dumping data for table `order_detail`
+----
+---- Dumping data for table `order_detail`
+----
 --
-
-LOCK TABLES `order_detail` WRITE;
-/*!40000 ALTER TABLE `order_detail` DISABLE KEYS */;
-/*!40000 ALTER TABLE `order_detail` ENABLE KEYS */;
-UNLOCK TABLES;
-
+--LOCK TABLES `order_detail` WRITE;
+--/*!40000 ALTER TABLE `order_detail` DISABLE KEYS */;
+--/*!40000 ALTER TABLE `order_detail` ENABLE KEYS */;
+--UNLOCK TABLES;
 --
--- Table structure for table `order_tb`
+----
+---- Table structure for table `order_tb`
+----
 --
-
-DROP TABLE IF EXISTS `order_tb`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `order_tb` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `created_by` varchar(255) DEFAULT NULL,
-  `created_date` datetime DEFAULT NULL,
-  `updated_by` varchar(255) DEFAULT NULL,
-  `updated_date` datetime DEFAULT NULL,
-  `order_state` varchar(255) DEFAULT NULL,
-  `order_total` double DEFAULT NULL,
-  `qty` int NOT NULL,
-  `billing_address_id` bigint DEFAULT NULL,
-  `payment_id` bigint DEFAULT NULL,
-  `user_id` bigint DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FKkciflckm4x5erskrpx2s5r2wq` (`billing_address_id`),
-  KEY `FK5i5rjxqe7dxok8u5mup1a7xjv` (`payment_id`),
-  KEY `FKatfyon9198am1ykhowffy7yrs` (`user_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
+--DROP TABLE IF EXISTS `order_tb`;
+--/*!40101 SET @saved_cs_client     = @@character_set_client */;
+--/*!50503 SET character_set_client = utf8mb4 */;
+--CREATE TABLE `order_tb` (
+--  `id` bigint NOT NULL AUTO_INCREMENT,
+--  `created_by` varchar(255) DEFAULT NULL,
+--  `created_date` datetime DEFAULT NULL,
+--  `updated_by` varchar(255) DEFAULT NULL,
+--  `updated_date` datetime DEFAULT NULL,
+--  `order_state` varchar(255) DEFAULT NULL,
+--  `order_total` double DEFAULT NULL,
+--  `qty` int NOT NULL,
+--  `billing_address_id` bigint DEFAULT NULL,
+--  `payment_id` bigint DEFAULT NULL,
+--  `user_id` bigint DEFAULT NULL,
+--  PRIMARY KEY (`id`),
+--  KEY `FKkciflckm4x5erskrpx2s5r2wq` (`billing_address_id`),
+--  KEY `FK5i5rjxqe7dxok8u5mup1a7xjv` (`payment_id`),
+--  KEY `FKatfyon9198am1ykhowffy7yrs` (`user_id`)
+--) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+--/*!40101 SET character_set_client = @saved_cs_client */;
 --
--- Dumping data for table `order_tb`
+----
+---- Dumping data for table `order_tb`
+----
 --
-
-LOCK TABLES `order_tb` WRITE;
-/*!40000 ALTER TABLE `order_tb` DISABLE KEYS */;
-/*!40000 ALTER TABLE `order_tb` ENABLE KEYS */;
-UNLOCK TABLES;
-
+--LOCK TABLES `order_tb` WRITE;
+--/*!40000 ALTER TABLE `order_tb` DISABLE KEYS */;
+--/*!40000 ALTER TABLE `order_tb` ENABLE KEYS */;
+--UNLOCK TABLES;
 --
--- Table structure for table `payment`
+----
+---- Table structure for table `payment`
+----
 --
-
-DROP TABLE IF EXISTS `payment`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `payment` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `created_by` varchar(255) DEFAULT NULL,
-  `created_date` datetime DEFAULT NULL,
-  `updated_by` varchar(255) DEFAULT NULL,
-  `updated_date` datetime DEFAULT NULL,
-  `active` bit(1) DEFAULT NULL,
-  `card_no` varchar(255) DEFAULT NULL,
-  `card_type` varchar(255) DEFAULT NULL,
-  `user_id` bigint DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK4spfnm9si9dowsatcqs5or42i` (`user_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
+--DROP TABLE IF EXISTS `payment`;
+--/*!40101 SET @saved_cs_client     = @@character_set_client */;
+--/*!50503 SET character_set_client = utf8mb4 */;
+--CREATE TABLE `payment` (
+--  `id` bigint NOT NULL AUTO_INCREMENT,
+--  `created_by` varchar(255) DEFAULT NULL,
+--  `created_date` datetime DEFAULT NULL,
+--  `updated_by` varchar(255) DEFAULT NULL,
+--  `updated_date` datetime DEFAULT NULL,
+--  `active` bit(1) DEFAULT NULL,
+--  `card_no` varchar(255) DEFAULT NULL,
+--  `card_type` varchar(255) DEFAULT NULL,
+--  `user_id` bigint DEFAULT NULL,
+--  PRIMARY KEY (`id`),
+--  KEY `FK4spfnm9si9dowsatcqs5or42i` (`user_id`)
+--) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+--/*!40101 SET character_set_client = @saved_cs_client */;
 --
--- Dumping data for table `payment`
+----
+---- Dumping data for table `payment`
+----
 --
-
-LOCK TABLES `payment` WRITE;
-/*!40000 ALTER TABLE `payment` DISABLE KEYS */;
-/*!40000 ALTER TABLE `payment` ENABLE KEYS */;
-UNLOCK TABLES;
-
+--LOCK TABLES `payment` WRITE;
+--/*!40000 ALTER TABLE `payment` DISABLE KEYS */;
+--/*!40000 ALTER TABLE `payment` ENABLE KEYS */;
+--UNLOCK TABLES;
 --
--- Table structure for table `product`
+----
+---- Table structure for table `product`
+----
 --
-
-DROP TABLE IF EXISTS `product`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `product` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `created_by` varchar(255) DEFAULT NULL,
-  `created_date` datetime DEFAULT NULL,
-  `updated_by` varchar(255) DEFAULT NULL,
-  `updated_date` datetime DEFAULT NULL,
-  `category` varchar(255) DEFAULT NULL,
-  `description` longtext,
-  `image_url` varchar(255) DEFAULT NULL,
-  `price` double DEFAULT NULL,
-  `title` varchar(255) DEFAULT NULL,
-  `cart_id` bigint DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK4dk3kwbu3jpiq1xxbmkasv9n3` (`cart_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
+--DROP TABLE IF EXISTS `product`;
+--/*!40101 SET @saved_cs_client     = @@character_set_client */;
+--/*!50503 SET character_set_client = utf8mb4 */;
+--CREATE TABLE `product` (
+--  `id` bigint NOT NULL AUTO_INCREMENT,
+--  `created_by` varchar(255) DEFAULT NULL,
+--  `created_date` datetime DEFAULT NULL,
+--  `updated_by` varchar(255) DEFAULT NULL,
+--  `updated_date` datetime DEFAULT NULL,
+--  `category` varchar(255) DEFAULT NULL,
+--  `description` longtext,
+--  `image_url` varchar(255) DEFAULT NULL,
+--  `price` double DEFAULT NULL,
+--  `title` varchar(255) DEFAULT NULL,
+--  `cart_id` bigint DEFAULT NULL,
+--  PRIMARY KEY (`id`),
+--  KEY `FK4dk3kwbu3jpiq1xxbmkasv9n3` (`cart_id`)
+--) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+--/*!40101 SET character_set_client = @saved_cs_client */;
 --
--- Dumping data for table `product`
+----
+---- Dumping data for table `product`
+----
 --
-
-LOCK TABLES `product` WRITE;
-/*!40000 ALTER TABLE `product` DISABLE KEYS */;
-/*!40000 ALTER TABLE `product` ENABLE KEYS */;
-UNLOCK TABLES;
-
+--LOCK TABLES `product` WRITE;
+--/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+--/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+--UNLOCK TABLES;
 --
--- Table structure for table `user`
+----
+---- Table structure for table `user`
+----
 --
-
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `user` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `created_by` varchar(255) DEFAULT NULL,
-  `created_date` datetime DEFAULT NULL,
-  `updated_by` varchar(255) DEFAULT NULL,
-  `updated_date` datetime DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+--DROP TABLE IF EXISTS `user`;
+--/*!40101 SET @saved_cs_client     = @@character_set_client */;
+--/*!50503 SET character_set_client = utf8mb4 */;
+--CREATE TABLE `user` (
+--  `id` bigint NOT NULL AUTO_INCREMENT,
+--  `created_by` varchar(255) DEFAULT NULL,
+--  `created_date` datetime DEFAULT NULL,
+--  `updated_by` varchar(255) DEFAULT NULL,
+--  `updated_date` datetime DEFAULT NULL,
+--  `email` varchar(255) DEFAULT NULL,
+--  `name` varchar(255) DEFAULT NULL,
+--  `password` varchar(255) DEFAULT NULL,
+--  PRIMARY KEY (`id`)
+--) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+--/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `user`
 --
 
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+--LOCK TABLES `user` WRITE;
+--/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+--/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+--UNLOCK TABLES;
 
 
 -- user1 =>$2a$10$n/8./7HF51jgCYaE7e1jHO8uWXqrm0aYypb/V9ObxnuDpafxO0HRe
